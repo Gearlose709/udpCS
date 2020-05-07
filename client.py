3463d7698730c035bce0a7fa950832c5feac60fa
709 <kovalevskij01@mail.ru>
2020-05-07 16:07:14 +0300

    2 commit

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 
+sock = socket.socket()
+sock.connect(('localhost', 9090))
 
 while True:
-    sock = socket.socket()
-    sock.connect(('localhost', 9090))
+
     sock.send(input().encode())
 
     data = sock.recv(1024)
-    sock.close()
+
 
     print (data.decode())
